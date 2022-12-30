72970aaad3dd8aeacdbc030935de37891c8560fe
Dionizio Gonzaga <dgonzaganeto@gmail.com>
2022-12-30 11:39:43 -0300

    adding another accounts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,47 @@
 fun main(args: Array<String>) {
     println("Bem Vindo ao Hofstadter Bank")
 
-    for(i in 1..10 step 2) {
-        val titular = "Dionzio $i"
-        val numeroConta = 1234 +i
-        val saldo = 100.00
+    val contaDionizio = Conta();
+    contaDionizio.titular = "Dionizio";
+    contaDionizio.numero = 1;
+    contaDionizio.saldo = 100000.0;
 
-        println("Titular: $titular")
-        println("Número da Conta: $numeroConta")
-        println("Saldo em conta: $saldo")
-        when {
-            saldo > 0.0 -> println("conta é positiva")
-            saldo == 0.0 -> println("conta é neutra")
-            else -> println("conta é negativa")
-        }
-        println()
-    }
+    val contaJessica = Conta();
+    contaJessica.titular = "Jessica";
+    contaJessica.numero = 2;
+    contaJessica.saldo = 100000.12;
+
+    println(contaDionizio.titular);
+    println(contaDionizio.numero);
+    println(contaDionizio.saldo);
+
+    println()
+
+    println(contaJessica.titular);
+    println(contaJessica.numero);
+    println(contaJessica.saldo);
+
 }
+
+class Conta{
+    var titular = "";
+    var numero = 0;
+    var saldo = 0.0;
+}
+
+
+//for(i in 1..10 step 2) {
+//    val titular = "Dionzio $i"
+//    val numeroConta = 1234 +i
+//    val saldo = 100.00
+//
+//    println("Titular: $titular")
+//    println("Número da Conta: $numeroConta")
+//    println("Saldo em conta: $saldo")
+//    when {
+//        saldo > 0.0 -> println("conta é positiva")
+//        saldo == 0.0 -> println("conta é neutra")
+//        else -> println("conta é negativa")
+//    }
+//    println()
+//}
