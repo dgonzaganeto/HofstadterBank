94499c26c322fc557d5ce079c0e80735c053e955
Dionizio Gonzaga <dgonzaganeto@gmail.com>
2023-01-02 16:03:36 -0300

    adding another accounts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,31 @@
 fun main(args: Array<String>) {
     println("Bem Vindo ao Hofstadter Bank")
+    println(clienteCorrentista())
+//    println(nomeTitular())
 
+//    println(qualidadeConta())
+}
+
+//println("Titular: $titular")
+//println("Número da Conta: $numeroConta")
+//println("Saldo em conta: $saldo")
+
+//fun qualidadeConta(){
+//    when{saldo > 0.0 -> println("conta é positiva")
+//    saldo == 0.0 -> println("conta é neutra")
+//    else -> println("conta é negativa")
+//}
+
+fun clienteCorrentista() {
     val contaDionizio = Conta()
     contaDionizio.titular = "Dionizio"
     contaDionizio.numero = 1
-    contaDionizio.saldo = 100000.0
+    contaDionizio.saldo = 1.00
 
     val contaJessica = Conta()
     contaJessica.titular = "Jessica"
     contaJessica.numero = 2
-    contaJessica.saldo = 100000.12
+    contaJessica.saldo = 10.00
 
     println(contaDionizio.titular)
     println(contaDionizio.numero)
@@ -21,6 +37,27 @@ fun main(args: Array<String>) {
     println(contaJessica.numero)
     println(contaJessica.saldo)
 
+    println()
+
+    println(contaDionizio.saldo)
+    println("Depositando na conta Dionizio")
+    deposita(contaDionizio, valor = 50.0)
+    println(contaDionizio.saldo)
+
+    println()
+
+    println("Saldo anteior conta de Jessica: ${contaJessica.saldo}")
+    println("Depositando na conta de Jessica")
+    deposita(contaJessica, valor = 100.0)
+    println("Saldo atual na conta de Jessica: ${contaJessica.saldo}")
+
+
+}
+
+fun deposita(conta: Conta, valor: Double){
+    conta.saldo += valor
+}
+fun nomeTitular(){
     val contaDidio = Conta()
     contaDidio.titular = "Dionizio"
 
@@ -30,27 +67,8 @@ fun main(args: Array<String>) {
     println("Titular conta Didio: ${contaDidio.titular}")
     println("Titular conta Jess: ${contaJess.titular}")
 }
-
-
-class Conta{
-    var titular = ""
-    var numero = 0
-    var saldo = 0.0
-}
-
-
-//for(i in 1..10 step 2) {
-//    val titular = "Dionzio $i"
-//    val numeroConta = 1234 +i
-//    val saldo = 100.00
-//
-//    println("Titular: $titular")
-//    println("Número da Conta: $numeroConta")
-//    println("Saldo em conta: $saldo")
-//    when {
-//        saldo > 0.0 -> println("conta é positiva")
-//        saldo == 0.0 -> println("conta é neutra")
-//        else -> println("conta é negativa")
-//    }
-//    println()
-//}
+    class Conta{
+        var titular = ""
+        var numero = 0
+        var saldo = 0.0
+    }
